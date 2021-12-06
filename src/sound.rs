b3d8b32b22fc4a997568f6759d908aadf853490c
Litchi Pi <litchi.pi@protonmail.com>
2021-12-06 14:30:42 +0100

    Adding TODOs
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

---
+++ src/sound.rs
@@ -0,0 +1,7 @@
+// TODO  Sound trait
+//          nb_samples()
+//          sampling_freq()
+//          get_sample(sample_ind: u32)
+
+// TODO     trait BackgroundMusic : Sound
+// TODO     trait SoundEffect : Sound
