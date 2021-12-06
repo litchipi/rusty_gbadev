217963c76a5b6ccfc387ddd36ec5aab42c2b1c34
Litchi Pi <litchi.pi@protonmail.com>
2021-12-06 08:29:37 +0100

    Adding TODOs
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

---
+++ src/screnes.rs
@@ -0,0 +1,18 @@
+// TODO Implement Scenes
+//      trait Scene
+//          Self::Identifier
+//
+//      Add the background
+//          TODO    Background trait
+//      Add the sprites
+//          TODO    Sprite trait
+//      Add the player interface
+//          TODO    PlayerInterface trait
+//
+//      Add teleportations points (to which scenes ?)
+//          Teleportation(ScreenCoord, SceneId)
+//      Add custom events
+//          set_custom_event(condition: Fn(&Game, &Scene) -> bool, fct: Fn(&mut System) -> ())
+//      Add sound
+//          // TODO     BackgroundMusic trait
+//          // TODO     SoundEffect     trait
